9ee689fc529d31cfa7ddc54bc48ba4b369b5259b
Nikhil Ajgaonkar <nikhil.aj24@gmail.com>
2021-03-15 21:05:34 -0400

    Add files via upload

---
+++ NN_Digits.py
@@ -0,0 +1,123 @@
+import numpy as np
+import pandas as pd
+import numpy as np
+from keras.datasets import mnist
+import random
+
+'''We will be traing an artificial neural network to recognize the hand written digits from the mnist data set. Since this is an artificial neural network, we will have to reshape each 2-D image into a 1-D array. The total number of pixels of each image will be the variables for our input layer. If its an (m,n) image then our input variables will be m*n. We will be using two hidden layers with sigmoid and softmax activation functions. For this example we are using 1000 nodes for the 1st hidden layer. We have set the learning rate (alpha) = 0.01 and we will run SGD for 200 iterations. To see if our code is working correctly or not, we will only use the 1st 1000 images from the train/test dataset.'''
+
+
+#Read the MNIST Data set
+(x0_train, y0_train), (x0_test, y0_test) = mnist.load_data()
+x0_train = x0_train[:1000,:,:]
+y0_train = y0_train[:1000]
+x0_test = x0_test[:1000,:,:]
+y0_test = y0_test[:1000]
+
+#Labels are 10 digits from 0-9 so we will perform one-hot encoding on the output.
+one_hot_labels_train = np.zeros((len(x0_train),10))
+one_hot_labels_test = np.zeros((len(x0_test),10))
+for i in range(len(one_hot_labels_train)):
+    one_hot_labels_train[i,y0_train[i]] = 1
+for i in range(len(one_hot_labels_test)):
+    one_hot_labels_test[i,y0_test[i]] = 1
+
+#Normalize the pixel values with min max scaling
+x_train = x0_train/np.max(x0_train)
+x_test = x0_test/np.max(x0_test)
+y_train = one_hot_labels_train
+y_test = one_hot_labels_test
+
+#Reshape the 2-D images into 1-D images
+x_train = np.reshape(x_train,(x_train.shape[0],int(x_train.shape[1]*x_train.shape[1])))
+x_test = np.reshape(x_test,(x_test.shape[0],int(x_test.shape[1]*x_test.shape[1])))
+
+
+#Define the Layers and nodes
+Hidden_Layers = 1
+Nodes_Hidden_Layers = 1000
+N_Layers = 1 + Hidden_Layers + 1 #Input + Hidden + Output
+Layers = np.zeros([N_Layers])
+Layers[0] = np.shape(x_train)[1]
+Layers[1] = Nodes_Hidden_Layers
+Layers[2] = 10
+
+#Initialize the weights depending on the hidden layers
+np.random.seed(42)
+W1 = np.random.randn(int(Layers[0]),int(Layers[1]))
+W2 = np.random.randn(int(Layers[1]),int(Layers[2]))
+
+
+#define activation functions and their derivatives
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+    
+def d_sigmoid(x):
+    return sigmoid(x)*(1-sigmoid(x))
+
+def softmax(A):
+    expA = np.exp(A)
+    return expA / expA.sum(axis=1, keepdims=True)
+
+def loss(y,yhat):
+    return np.sum(-y*np.log(yhat))
+    
+    
+#set the learning rate (alpha), number of iterations and initialize cost function array
+alpha = 0.01
+epochs = 200
+Loss_function = []
+#Start the stochastic gradient descent method
+for i in range (0,epochs):
+    #forward propagation
+    Z1 = np.dot(x_train,W1)
+    A1 = sigmoid(Z1)
+    Z2 = np.dot(A1,W2)
+    y_pred = softmax(Z2)
+    loss_f = loss(y_train,y_pred)
+    Loss_function.append(loss_f.sum())
+    #Back propagation
+    dJ_dW2 = np.dot(A1.T,(y_pred-y_train))
+    dZ1_dW1 = x_train
+    dA1_dZ1 = d_sigmoid(Z1)
+    dJ_dZ2 = (y_pred-y_train)
+    dZ2_dA1 = W2
+    dJ_dA1 = np.dot(dJ_dZ2,dZ2_dA1.T)
+    dJ_dZ1 = dJ_dA1*dA1_dZ1
+    dJ_dW1 = np.dot(dZ1_dW1.T,dJ_dZ1)
+    #update weights
+    W1 -= alpha*dJ_dW1
+    W2 -= alpha*dJ_dW2
+    
+#Define the prediction function
+def predict(X,W1,W2):
+    Z1 = np.dot(X,W1)
+    A1 = sigmoid(Z1)
+    Z2 = np.dot(A1,W2)
+    y_pred = softmax(Z2)
+    y_new = np.zeros(np.shape(y_pred))
+    #Convert softmax output to 0s and 1s
+    for num in range(len(y_pred)):
+        y_new[num,np.argmax(y_pred[num,:])] = 1
+    return y_new
+    
+#calculate accuracy
+def acc(y, yhat):
+    acc = (sum(y == yhat) / len(y) * 100)
+    return acc
+
+#predict test and train accuracy
+y_pred_test = predict(x_test,W1,W2)
+y_pred_train = predict(x_train,W1,W2)
+
+test_ac = np.mean(acc(y_test,y_pred_test))
+train_ac = np.mean(acc(y_train,y_pred_train))
+
+
+print("Train accuracy is " + str(train_ac))
+print("Test accuracy is " + str(test_ac))
+
+#You can plot the Loss_function as a function of iterations to see if it is minimized.
+#You can play with different values of iterations, learning rate and hidden layer nodes to see if your accuracy improves
+
+'''Optional: Another way to optimize this neural network is to run a Markov Chain Monte Carlo random walk in the 4-D parameter space of iterations, learning rate, nodes and number of hidden layers for N number of steps. At each step, use the trial parameters  and train the NN on the dataset, compute the accuracy and accept the trail parameters if the accuracy is better than the previous step. You can used Metropolis-Hasting Sampling to sample the parameter space'''
---
+++ NN_heart_disease.py
@@ -0,0 +1,115 @@
+import numpy as np
+import pandas as pd
+
+'''We will use ANN to predict if the patient is prone to heart disease or not given a set of input parameters like age,sex, blood pressure etc. These will be our input parameters for our input layer. We will be using a 2 Layer ANN with ReLU and Sigmoid activation functions. This ANN is parametrized by specific values of learning rate (alpha), number of iterations and number of nodes for the hidden layer. You can use different values for these parameters and see if you get a better accuracy.'''
+
+#Perform standard scaling on the data set
+def StandardScaler(Data):
+    for feature in range (0,Data.shape[1]-1):
+        Current_feature = Data.iloc[:,feature]
+        feature_std = (Current_feature - np.mean(Data.iloc[:,feature]))/(np.std(Data.iloc[:,feature]))
+        Data.iloc[:,feature].update(feature_std)
+    return Data
+    
+#Name the headers
+headers =  ['age', 'sex','chest_pain','resting_blood_pressure','serum_cholestoral','fasting_blood_sugar','resting_ecg_results','max_heart_rate_achieved','exercise_induced_angina','oldpeak',"slope of the peak",'num_of_major_vessels','thal', 'heart_disease']
+#Read the data file
+df = pd.read_csv('/Users/nikhil/Data/ML_examples/heart.dat',sep=' ',names=headers)
+df.isna().sum()#check for NaN
+df.dtypes#check for data types. Encode to numeric if catagorical variables are involved
+df = StandardScaler(df)
+#Replace the heart-disease value of 1,2 to 1,0
+df['heart_disease']=df['heart_disease'].replace(to_replace=[1,2], value=[0, 1])
+
+#Shuffle data set
+shuffle_df = df.sample(frac=1)
+train_size = int(0.7 * len(df))
+#Split it in train and test set
+train_df = shuffle_df[:train_size]
+test_df = shuffle_df[train_size:]
+
+#Split into target and independent variables
+y_train = np.array(train_df['heart_disease'])
+y_test = np.array(test_df['heart_disease'])
+x_train = np.array(train_df.drop(columns=['heart_disease']))
+x_test =np.array(test_df.drop(columns=['heart_disease']))
+#Add ones as the last column in x_train and x_test to take care of the intercept. This way you dont have to update b values separately.
+x_train = np.c_[x_train,np.ones(np.shape(x_train)[0])]
+x_test = np.c_[x_test,np.ones(np.shape(x_test)[0])]
+
+#Set Hidden Layers and nodes for NN
+Hidden_Layers = 1
+Nodes_Hidden_Layers = 10
+N_Layers = 1 + Hidden_Layers + 1 #Input + Hidden + Output
+Layers = np.zeros([N_Layers])
+Layers[0] = np.shape(x_train)[1]
+Layers[1] = Nodes_Hidden_Layers
+Layers[2] = 1
+
+#Initialize weights
+#np.random.seed(1)
+W1 = np.random.randn(int(Layers[0]),int(Layers[1]))
+W2 = np.random.randn(int(Layers[1]),int(Layers[2]))
+
+#Define activation functions
+def relu(x):
+    x[x<0] = 0
+    return x
+    
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+    
+def d_sigmoid(x):
+    return sigmoid(x)*(1-sigmoid(x))
+    
+def loss(y,y_pred):
+    return 0.5*((y_pred - y)**2)
+    
+def dRelu(x):
+    x[x<0] = 0
+    x[x>0] = 1
+    return x
+
+
+#Initialize Cost function array. Set number of iterations and learning rate (alpha)
+Loss_function = []
+epochs = 500
+alpha = 0.0001
+for i in range (0,epochs):
+    #Forward propagation
+    Z1 = np.dot(x_train,W1)
+    A1 = relu(Z1)
+    Z2 = np.dot(A1,W2)
+    y_pred = sigmoid(Z2)[:,0]
+    Loss = loss(y_train,y_pred)
+    Loss_function.append(Loss.sum())
+    #Backward porpagation
+    dloss_dW2 = np.dot((y_pred - y_train)*d_sigmoid(Z2)[:,0],A1)
+    dloss_dW1 = np.dot((np.dot(((y_pred - y_train)*d_sigmoid(Z2)).T,dRelu(Z1))).T,x_train)
+    dloss_dW2 = dloss_dW2.reshape(1,-1)
+    #Update weights
+    W1 -= (alpha*dloss_dW1.T)
+    W2 -= (alpha*dloss_dW2.T)
+    
+
+def predict(X,W1,W2):
+    Z1 = np.dot(X,W1)
+    A1 = relu(Z1)
+    Z2 = np.dot(A1,W2)
+    y_pred = sigmoid(Z2)
+    return np.round(y_pred)
+
+
+def acc(y, yhat):
+    acc = (sum(y == yhat) / len(y) * 100)
+    return acc
+    
+y_pred_test = predict(x_test,W1,W2)[:,0]
+y_pred_train = predict(x_train,W1,W2)[:,0]
+
+test_ac = acc(y_test,y_pred_test)
+train_ac = acc(y_train,y_pred_train)
+
+print("Train accuracy is " + str(train_ac))
+print("Test accuracy is " + str(test_ac))
+
